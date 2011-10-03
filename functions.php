2e390459623571fc32a2ddd2cd615f61ca507bb0
Max Malm <benjick@dumfan.net>
2011-10-03 13:47:06 +0200

    Signed-off-by: Max Malm <benjick@dumfan.net>

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * functions.php for wp-torrent *
+ */
+
+require_once('functions/shortcode.php'); // load the master shortcode file
+require_once('functions/widgets.php'); // load the master widgets file
---
+++ functions/widgets.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once('widgets/torrent-latest.php'); // Widget: Latest Torrents
