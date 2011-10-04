4121b0072c5678ea7ff4fbd9e801649d8fa9ae6d
Max Malm <benjick@dumfan.net>
2011-10-04 11:27:15 +0200

    from theme twentyeleven (stripped)

---
+++ wp-torrent/footer.php
@@ -0,0 +1,14 @@
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+
+			<?php
+				get_sidebar( 'footer' );
+			?>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
