8319e26a2f47c71f128cbf806a146ac36ece5b9a
Max Malm <benjick@dumfan.net>
2011-10-05 14:32:49 +0200

    Added widgetsupport

--- wp-torrent/functions/widgets.php
+++ wp-torrent/functions/widgets.php
@@ -1,5 +1,35 @@
 <?php
 
+function wptorrent_footer_sidebar_class() { // from twentyeleven
+	$count = 0;
+
+	if ( is_active_sidebar( 'sidebar-3' ) )
+		$count++;
+
+	if ( is_active_sidebar( 'sidebar-4' ) )
+		$count++;
+
+	if ( is_active_sidebar( 'sidebar-5' ) )
+		$count++;
+
+	$class = '';
+
+	switch ( $count ) {
+		case '1':
+			$class = 'one';
+			break;
+		case '2':
+			$class = 'two';
+			break;
+		case '3':
+			$class = 'three';
+			break;
+	}
+
+	if ( $class )
+		echo 'class="' . $class . '"';
+}
+
 function wptorrent_widgets_init() {
 
 	#register_widget( 'torrent-latest' );
@@ -11,6 +41,35 @@ function wptorrent_widgets_init() {
 		'after_widget' => "</aside>\n",
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>' . "\n",
+	) );
+	register_sidebar( array(
+		'name' => __( 'Footer Area One', 'wp-torrent' ),
+		'id' => 'sidebar-3',
+		'description' => __( 'An optional widget area for your site footer', 'wp-torrent' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Footer Area Two', 'wp-torrent' ),
+		'id' => 'sidebar-4',
+		'description' => __( 'An optional widget area for your site footer', 'wp-torrent' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Footer Area Three', 'wp-torrent' ),
+		'id' => 'sidebar-5',
+		'description' => __( 'An optional widget area for your site footer', 'wp-torrent' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'wptorrent_widgets_init' );
