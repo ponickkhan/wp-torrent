cbdb08683cb9f9d046f3afb2a1c19d3126e291d7
Max Malm <benjick@dumfan.net>
2011-10-06 15:47:32 +0200

    added content_nav from twentyeleven

--- wp-torrent/functions.php
+++ wp-torrent/functions.php
@@ -107,4 +107,16 @@ function auth_redirect_login() {
     }
 }
 
+function content_nav( $nav_id ) {
+	global $wp_query;
+
+	if ( $wp_query->max_num_pages > 1 ) : ?>
+		<nav id="<?php echo $nav_id; ?>">
+			<h3 class="assistive-text"><?php _e( 'Post navigation', 'wp-torrent' ); ?></h3>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'wp-torrent' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'wp-torrent' ) ); ?></div>
+		</nav><!-- #nav-above -->
+	<?php endif;
+}
+
 # end 
